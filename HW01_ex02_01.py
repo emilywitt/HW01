502b48ee28b97ce14cb531838ad888cf811ad60a
emilywitt <emily.witt@ischool.berkeley.edu>
2015-08-04 13:01:59 -0700

    x=5

--- HW01_ex02_01.py
+++ HW01_ex02_01.py
@@ -7,20 +7,27 @@
 # (2) Now put the same statements into a script [this script] and run it. 
 ######## enter statements below this line ########
 
-print("Hello World!") # example
-
-
-
-
-
+5
+x=5
+x+1
 
 
 
 ######## enter statements above this line ########
 # What is the output?
-# [copy and past output here]
+# nothing happens: calvisitor-10-105-188-126:HW01 emilywitt$
 # 
 # (3) Modify the script by transforming each expression into a print statement 
 # and then run it again.
 # What is the output?
-# [copy and past output here]
+# calvisitor-10-105-188-126:HW01 emilywitt$ python HW01_ex02_01.py
+# File "HW01_ex02_01.py", line 11
+#    print x=5
+#           ^
+# SyntaxError: invalid syntax #
+# when I do
+# print 5
+# x = 5
+#print x+1 I get the output:
+#5
+#6
