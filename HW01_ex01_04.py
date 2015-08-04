b618662250d65d7eeb06b33f549b9f6df483b092
emilywitt <emily.witt@ischool.berkeley.edu>
2015-08-04 12:28:24 -0700

    hw1

--- HW01_ex01_04.py
+++ HW01_ex01_04.py
@@ -1,11 +1,12 @@
 # HW01_ex01_04
 # Start the Python interpreter and use it as a calculator. 
-# Python’s syntax for math operations is almost the same as 
+# Python's syntax for math operations is almost the same as 
 # standard mathematical notation. For example, the symbols 
 # +, - and / denote addition, subtraction and division, as you would expect. 
 # The symbol for multiplication is *.
 
-# If you run a 10 kilometer race in 43 minutes 30 seconds, what is your 
-# average time per mile? What is your average speed in miles per hour? 
+# If you run a 10 kilometer race in 43 minutes 30 seconds 6.2 miles in 43.5 minutes
+# what is your average time per mile?  7.02 minutes per mile
+# What is your average speed in miles per hour? 8.5 miles per hour
 # (Hint: there are 1.61 kilometers in a mile).
-# Average Speed in MPH:
+# Average Speed in MPH: 8.5 miles per hour
